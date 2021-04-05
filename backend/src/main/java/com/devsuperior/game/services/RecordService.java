cc44fdc56fc9e52bf90c5dfd6033aea28ff60bba
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-04-05 10:14:01 -0300

    POST Record

---
+++ backend/src/main/java/com/devsuperior/game/services/RecordService.java
@@ -0,0 +1,36 @@
+package com.devsuperior.game.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.game.entities.Game;
+import com.devsuperior.game.entities.Record;
+import com.devsuperior.game.entities.dto.RecordDTO;
+import com.devsuperior.game.entities.dto.RecordInsertDTO;
+import com.devsuperior.game.repositories.GameRepository;
+import com.devsuperior.game.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Game game = gameRepository.getOne(dto.getGameId());
+		Record entity = new Record();
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		entity.setGame(game);
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+	}
+}
